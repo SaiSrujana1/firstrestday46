2657d7fa2a0aa422033869ecf3cc42a698addd2e
saisrujana <saisrujana.nalam@jobiak.com>
2022-05-17 11:42:59 +0530

    Done

---
+++ firstrestday46/src/main/java/com/jobiak/empapi/service/MobileService.java
@@ -0,0 +1,29 @@
+package com.jobiak.empapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jobiak.empapi.model.Mobile;
+import com.jobiak.empapi.repository.MobileRepository;
+
+@Service
+public class MobileService {
+
+	@Autowired
+	MobileRepository repo;
+	
+	public Mobile addMobile(Mobile mobile) {
+		Mobile ref = repo.save(mobile);
+		return ref;
+	}
+
+	public Mobile modifyMobile(Mobile mobile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void removeProduct(Long mobileid) {
+		// TODO Auto-generated method stub
+		
+	}
+}
